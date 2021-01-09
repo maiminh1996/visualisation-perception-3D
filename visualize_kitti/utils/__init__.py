863333ddf7827fbd2e5f80fc7d6006295c1b4869
mai minh <nguyen.mai@insa-cvl.fr>
2021-01-09 11:43:03 +0100

    modif moyen import package

--- visualize_kitti/utils/__init__.py
+++ visualize_kitti/utils/__init__.py
@@ -1,2 +1,5 @@
 from .kitti_object import show_image_with_boxes, show_lidar_with_boxes
 from .kitti_object import show_lidar_topview_with_boxes, show_lidar_on_image
+from .kitti_object import kitti_object
+
+from .viz_util import draw_lidar_simple, draw_lidar
--- visualize_kitti/visua.py
+++ visualize_kitti/visua.py
@@ -6,15 +6,11 @@ import cv2
 if "mlab" not in sys.modules:
     import mayavi.mlab as mlab
 
-MY_DIRNAME = os.path.dirname(os.path.abspath(__file__))  # take the current dir
-PARENT_DIRNAME = os.path.abspath(os.path.join(MY_DIRNAME, os.pardir))  # take the dir for needed import fold
-sys.path.insert(0, PARENT_DIRNAME)
-
-from utils.kitti_object import kitti_object
+from utils import kitti_object
 from utils import show_image_with_boxes, show_lidar_with_boxes
 from utils import show_lidar_on_image, show_lidar_topview_with_boxes
 
-from viz_util import draw_lidar_simple, draw_lidar, draw_gt_boxes3d
+from  utils import draw_lidar_simple, draw_lidar
 
 def main(path, data_idx):
     """
@@ -40,14 +36,14 @@ def main(path, data_idx):
     calib = dataset.get_calibration(data_idx)
     pc_velo = dataset.get_lidar(data_idx)  # (nx4) with 4: x,y,z,r
 
-    # draw_lidar_simple(pc_velo[:,0:3])
+    draw_lidar_simple(pc_velo[:,0:3])
     # draw_lidar(pc_velo) # go to if color is None: color = pc[:, 0] to change color intensity or z
-    show_lidar_with_boxes(pc_velo[:, 0:3], objects, calib, True, img_width, img_height)
+    # show_lidar_with_boxes(pc_velo[:, 0:3], objects, calib, True, img_width, img_height)
     # show_image_with_boxes(img, objects, calib, True)
     # show_lidar_topview_with_boxes(pc_velo, objects, calib)  # TODO recode and understand
     # show_lidar_on_image(pc_velo[:, 0:3], img, calib, img_width, img_height, side='left')
 
 if __name__ == '__main__':
     dataset_path = "../dataset/kitti"
-    data_idx = 16 #85: #20, 31, 47
+    data_idx = 20 #85: #20, 31, 47
     main(dataset_path, data_idx)
