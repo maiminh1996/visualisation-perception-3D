8f5a3f9174853e6c2519335e7f71aec63064dc9d
mai minh <nguyen.mai@insa-cvl.fr>
2021-01-09 11:56:46 +0100

    add load scan

--- visualize_kitti/utils/__init__.py
+++ visualize_kitti/utils/__init__.py
@@ -2,4 +2,6 @@ from .kitti_object import show_image_with_boxes, show_lidar_with_boxes
 from .kitti_object import show_lidar_topview_with_boxes, show_lidar_on_image
 from .kitti_object import kitti_object
 
+from .kitti_util import load_velo_scan
+
 from .viz_util import draw_lidar_simple, draw_lidar
