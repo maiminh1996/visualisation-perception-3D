082246a62cbd3fed8d2173322e2f26fbae1c5686
Minh <mainguyenanhminh1996@gmail.com>
2021-01-11 00:11:11 +0100

    1st commit

--- inverse_projection_open3d/visua_pcl.py
+++ inverse_projection_open3d/visua_pcl.py
@@ -20,21 +20,22 @@ if __name__ == "__main__":
     vis.create_window()
     vis.add_geometry(pcd)
 
-    # render_option = vis.get_render_option()
-    # render_option.point_size = 0.01
+    render_option = vis.get_render_option()
+    render_option.point_size = 0.1
 
     data = pykitti.raw(basedir, date, drive)
     to_reset_view_point = True
-    for points_with_intensity in data.velo:
-        points = points_with_intensity[:, :3]
-        pcd.points = open3d.Vector3dVector(points)
+    while True:
+        for points_with_intensity in data.velo:
+            points = points_with_intensity[:, :3]
+            pcd.points = open3d.Vector3dVector(points)
 
-        vis.update_geometry()
-        if to_reset_view_point:
-            vis.reset_view_point(True)
-            to_reset_view_point = False
-        vis.poll_events()
-        vis.update_renderer()
-        time.sleep(0.2)
+            vis.update_geometry()
+            if to_reset_view_point:
+                vis.reset_view_point(True)
+                to_reset_view_point = False
+            vis.poll_events()
+            vis.update_renderer()
+            time.sleep(0.2)
 
     vis.destroy_window()
