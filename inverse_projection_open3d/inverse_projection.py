e9c0d805246bde580870d5a5cc0414af9a98cb79
Minh <mainguyenanhminh1996@gmail.com>
2021-01-13 23:50:42 +0100

    add calib from 000000.txt object detection file

--- inverse_projection_open3d/inverse_projection.py
+++ inverse_projection_open3d/inverse_projection.py
@@ -3,8 +3,8 @@ import open3d as o3d
 import matplotlib.pyplot as plt
 import numpy as np
 from geometry_utils import *
-from utils import load_depth_png, load_image, Calibration
-from utils import load_from_bin
+from utils import load_depth_png, load_image, Calibration, Calibration0
+from utils import load_from_bin, transform_original_image
 import time, os, sys
 from tqdm import tqdm
 
@@ -17,12 +17,133 @@ sys.path.insert(0, os.path.join(MY_DIRNAME, '..'))
 from visualize_kitti.utils import get_lidar_in_image_fov
 # from visualize_kitti.utils import Calibration
 
+def show_depth(depth):
+    from PIL import Image
+    from matplotlib import cm
+    import matplotlib.pyplot as plt
+    im = Image.fromarray(np.uint8(depth))  # .convert('RGB')
+    # im = Image.fromarray(np.uint8(cm.gist_earth(depth_map)))
+    # im.save('/home/zelt/Desktop/books_read.png')
+    plt.imshow(im)# , vmin=0, vmax=5)
+    # plt.imsave('/home/zelt/Desktop/test/4_beams_right.png', im, vmin=0, vmax=30)  # , cmap='hot')
+    plt.show()
+
 def main(img_path, depth_path):
     # load image, depth
     rgb = load_image(img_path)
+    rgb = transform_original_image(rgb)
     depth = load_depth_png(depth_path)
+
+    # show_depth(depth)
+    
     print(np.count_nonzero(depth!=0))
     
+    # Get intrinsic parameters from field of view fov
+    height, width, _ = rgb.shape
+    K = intrinsic_from_fov(height, width, 90)  # +- 45 degrees
+    K_inv = np.linalg.inv(K)
+    # Get pixel coordinates
+    pixel_coords = pixel_coord_np(width, height)  # [3, npoints]
+    # Apply back-projection: K_inv @ pixels * depth
+    cam_coords = K_inv[:3, :3] @ pixel_coords * depth.flatten()
+
+    rgb = rgb.reshape((height*width, 3)).transpose()
+    # Limit points to 150m in the z-direction for visualisation
+    rgb = rgb[:, np.where(cam_coords[2] <= 150)[0]]
+    cam_coords = cam_coords[:, np.where(cam_coords[2] <= 150)[0]]
+    # rgb = rgb[:, np.where(cam_coords[1] >= 1.8)[0]]
+    # cam_coords = cam_coords[:, np.where(cam_coords[1] >= 1.8)[0]]
+    # Limit points to 3m hauteur in the x-direction for visualisation 
+    rgb = rgb[:, np.where(cam_coords[1] >= -1)[0]] 
+    cam_coords = cam_coords[:, np.where(cam_coords[1] >= -1)[0]]
+
+    def custom_draw_geometry(pcd, fov_step):
+        # http://www.open3d.org/docs/0.9.0/tutorial/Advanced/customized_visualization.html
+        vis = o3d.visualization.Visualizer()
+        vis.create_window()
+        
+        render_option = vis.get_render_option()
+        render_option.point_size = 0.1
+        
+        vis.add_geometry(pcd)
+
+        ctr = vis.get_view_control()
+        # print("Field of view (before changing) %.2f" % ctr.get_field_of_view())
+        ctr.change_field_of_view(step=fov_step)
+        
+        vis.run()
+        vis.destroy_window()
+
+    def change_background_to_black(vis):
+        opt = vis.get_render_option()
+        # opt.background_color = np.asarray([0.1, 0.1, 0.1])
+        return False
+
+    # Visualize
+    pcd_cam = o3d.geometry.PointCloud()
+
+    # pcd_cam.points = o3d.utility.Vector3dVector(cam_coords.T[:, :3])
+    pcd_cam.points = o3d.Vector3dVector(cam_coords.T[:, :3])
+    # Flip it, otherwise the pointcloud will be upside down
+    pcd_cam.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    # pcd_cam.colors  = o3d.utility.Vector3dVector(rgb.T.astype('float64') / 255.0)
+    pcd_cam.colors  = o3d.Vector3dVector(rgb.T.astype('float64') / 255.0)
+    
+    ### open3d > 0.10.0 ubuntu 18
+    # cl, ind = pcd_cam.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+    # inlier_cloud = pcd_cam.select_by_index(ind)
+
+    #cl1, ind1 = pcd_cam.remove_radius_outlier(nb_points=5, radius=0.5)
+    ### open3d 0.7.0 ubuntu 16.04
+    # cl, ind = o3d.geometry.statistical_outlier_removal(pcd_cam, nb_neighbors=20, std_ratio=2.0)
+    # inlier_cloud = pcd_cam.select_by_index(ind) not found in 0.7.0
+    custom_draw_geometry(pcd_cam, 90.0)
+    # o3d.visualization.draw_geometries_with_animation_callback([pcd_cam], change_background_to_black)
+
+def sparse0(img_path, lidar_path, calib_path):
+    # load image, depth
+    rgb = load_image(img_path)
+    img_height, img_width, img_channel = rgb.shape
+    calib = Calibration0(calib_path)
+    # depth = load_depth_png(lidar_path)
+    pc_velo = load_from_bin(lidar_path)
+    ######################
+    imgfov_pc_velo, pts_2d, fov_inds = get_lidar_in_image_fov(pc_velo[:, :3], calib, 0, 0, img_width, img_height, True)
+
+    imgfov_pts_2d = pts_2d[fov_inds, :]
+    print(imgfov_pts_2d.shape)
+
+    # project those 3d pcl in velo coord to rect cam coord
+    imgfov_pc_rect = calib.project_velo_to_rect(imgfov_pc_velo)
+
+    import matplotlib.pyplot as plt
+
+    cmap = plt.cm.get_cmap("hsv", 256)
+    cmap = np.array([cmap(i) for i in range(256)])[:, :3] * 255
+
+
+    img_test = np.zeros([rgb.shape[0], rgb.shape[1]])
+
+    for i in tqdm(range(imgfov_pts_2d.shape[0])):
+        depth = imgfov_pc_rect[i, 2]
+        # print(img_test.shape)
+        # print("{}, {}".format(int(np.round(imgfov_pts_2d[i, 1])), int(np.round(imgfov_pts_2d[i, 0]))))
+        img_test[int(np.round(imgfov_pts_2d[i, 1]))-1, int(np.round(imgfov_pts_2d[i, 0]))-1] = depth
+    depth = img_test
+    print(np.count_nonzero(depth!=0))
+    
+    
+    from PIL import Image
+    from matplotlib import cm
+    import matplotlib.pyplot as plt
+    im = Image.fromarray(np.uint8(img_test))  # .convert('RGB')
+    # im = Image.fromarray(np.uint8(cm.gist_earth(depth_map)))
+    # im.save('/home/zelt/Desktop/books_read.png')
+    plt.imshow(im, vmin=0, vmax=5)
+    # plt.imsave('/home/zelt/Desktop/test/4_beams_right.png', im, vmin=0, vmax=30)  # , cmap='hot')
+    plt.show()
+    
+    ######################        
     # Get intrinsic parameters from field of view fov
     height, width, _ = rgb.shape
     K = intrinsic_from_fov(height, width, 90)  # +- 45 degrees
@@ -39,6 +160,8 @@ def main(img_path, depth_path):
     def custom_draw_geometry(pcd):
         vis = o3d.visualization.Visualizer()
         vis.create_window()
+        render_option = vis.get_render_option()
+        # render_option.point_size = 0.01
         vis.add_geometry(pcd)
         vis.run()
         vis.destroy_window()
@@ -101,7 +224,7 @@ def sparse(img_path, lidar_path, velo_to_cam, cam_to_cam):
     depth = img_test
     print(np.count_nonzero(depth!=0))
     
-    """
+    
     from PIL import Image
     from matplotlib import cm
     import matplotlib.pyplot as plt
@@ -111,7 +234,7 @@ def sparse(img_path, lidar_path, velo_to_cam, cam_to_cam):
     plt.imshow(im, vmin=0, vmax=5)
     # plt.imsave('/home/zelt/Desktop/test/4_beams_right.png', im, vmin=0, vmax=30)  # , cmap='hot')
     plt.show()
-    """
+    
     ######################        
     # Get intrinsic parameters from field of view fov
     height, width, _ = rgb.shape
@@ -163,6 +286,8 @@ def video(img_list, depth_list):
     pcd = o3d.PointCloud()
     vis = o3d.Visualizer()
     vis.create_window()
+    opt = vis.get_render_option()
+    opt.background_color = np.asarray([0.1, 0.1, 0.1])
     vis.add_geometry(pcd)
     # render_option = vis.get_render_option()
     # render_option.point_size = 0.1
@@ -172,7 +297,7 @@ def video(img_list, depth_list):
     Cam_coords = []
     Rgb = []
     
-    for i in range(len(img_list)):
+    for i in tqdm(range(len(img_list))):
         # load image, depth
         rgb = load_image(img_list[i])
         depth = load_depth_png(depth_list[i])
@@ -202,7 +327,7 @@ def video(img_list, depth_list):
                 to_reset_view_point = False
             vis.poll_events()
             vis.update_renderer()
-            time.sleep(0.2)
+            time.sleep(0.1)
     vis.destroy_window()
 
 def video_sparse(img_list, lidar_list, velo_to_cam, cam_to_cam):
@@ -301,11 +426,21 @@ if __name__ == "__main__":
     # 5-69
     # img_path = "../dataset/kitti/raw_data/2011_09_28_drive_0106_sync/image_2/"
     # depth_path = "../dataset/kitti/raw_data/2011_09_28_drive_0106_sync/groundtruth_depth/"
-    img_path = "../dataset/kitti/raw_data/2011_09_28/2011_09_28_drive_0106_sync/image_02/data/"
-    lidar_path = "../dataset/kitti/raw_data/2011_09_28/2011_09_28_drive_0106_sync/velodyne_points/data/"
-    depth_path = "../dataset/kitti/raw_data/data_depth_annotated/train/2011_09_28_drive_0106_sync/proj_depth/groundtruth/image_02/"
-    img_list = [img_path + '%010d.png' % (i) for i in range(5, 70)] 
-    lidar_list = [lidar_path + '%010d.bin' % (i) for i in range(5, 70)] 
-    depth_list = [depth_path + '%010d.png' % (i) for i in range(5, 70)] 
-    video(img_list, depth_list)
+    img_path = "../dataset/kitti/raw_data/2011_09_26/2011_09_26_drive_0032_sync/image_02/data/"
+    lidar_path = "../dataset/kitti/raw_data/2011_09_26/2011_09_26_drive_0032_sync/velodyne_points/data/"
+    depth_path = "../dataset/kitti/raw_data/data_depth_annotated/train/2011_09_26_drive_0032_sync/proj_depth/groundtruth/image_02/"
+    img_list = [img_path + '%010d.png' % (i) for i in range(5, 375)] 
+    lidar_list = [lidar_path + '%010d.bin' % (i) for i in range(5, 375)] 
+    depth_list = [depth_path + '%010d.png' % (i) for i in range(5, 375)] 
+    # video(img_list, depth_list)
     # video_sparse(img_list, lidar_list, calib_velo_to_cam, calib_cam_to_cam)
+
+    img_path = "../dataset/kitti/training/image_2/000085.png"
+    # img_path = "../dataset/kitti/training/image_beta0.03745/000085.png"
+    depth_path = "../dataset/kitti/training/depth_spaded/000020.png"
+    
+    lidar_path = "../dataset/kitti/training/velodyne/000085.bin"
+    # lidar_path = "../dataset/kitti/training/velodyne_points_beta0.03745/000085.bin"
+    calib_path = "../dataset/kitti/training/calib/000085.txt"
+    # main(img_path, depth_path)
+    sparse0(img_path, lidar_path, calib_path)
--- inverse_projection_open3d/utils.py
+++ inverse_projection_open3d/utils.py
@@ -14,9 +14,12 @@ def load_depth_png(depth_path):
 def load_from_bin(bin_path):
     obj = np.fromfile(bin_path, dtype=np.float32)
     # print(obj.shape)
-    obj = obj.reshape(-1, 4)
+    try:
+        obj = obj.reshape(-1, 4)
+    except ValueError:
+        obj = obj.reshape(-1, 5)
     # print(obj.shape)
-    
+
     return obj
 
 
@@ -27,6 +30,34 @@ def load_image(img_filename):
     img = cv2.imread(img_filename)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # left
     return img
+
+def transform_original_image(img):
+    # Crop-center original image
+    cropx, cropy = 1216, 352
+    y, x, _ = img.shape
+    startx = x // 2 - (cropx // 2)
+    starty = y // 2 - (cropy // 2)
+    return img[starty:starty + cropy, startx:startx + cropx]
+
+def load_image1(img_filename):
+    try:
+        from PIL import Image
+    except ImportError:
+        import Image
+
+    im = Image.open(img_filename)
+    width, height = im.size   # Get dimensions
+    new_width = 1216
+    new_height = 352
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
+
+    # Crop the center of the image
+    im = im.crop((left, top, right, bottom))
+    return np.array(im)
+
 
 class Calibration(object):
     """
@@ -309,3 +340,230 @@ def inverse_rigid_trans(Tr):
 
 # https://github.com/windowsub0406/KITTI_Tutorial/blob/master/velo2cam_projection.ipynb
 # https://programtalk.com/vs2/python/13601/pykitti/pykitti/raw.py/
+
+
+
+class Calibration0(object):
+    """
+    Calibration matrices and utils
+    3d XYZ in <label>.txt are in rect camera coord.
+    2d box xy are in image2 coord
+    Points in <lidar>.bin are in Velodyne coord.
+
+    y_image2 = P^2_rect * x_rect
+    y_image2 = P^2_rect * R0_rect * Tr_velo_to_cam * x_velo
+    x_ref = Tr_velo_to_cam * x_velo
+    x_rect = R0_rect * x_ref
+
+    P^2_rect = [f^2_u,  0,      c^2_u,  -f^2_u b^2_x;
+                0,      f^2_v,  c^2_v,  -f^2_v b^2_y;
+                0,      0,      1,      0]
+             = K * [1|t]
+
+    image2 coord:
+     ----> x-axis (u)
+    |
+    |
+    v y-axis (v)
+
+    velodyne coord:
+    front x, left y, up z
+
+    rect/ref camera coord:
+    right x, down y, front z
+
+    Ref (KITTI paper): http://www.cvlibs.net/publications/Geiger2013IJRR.pdf
+
+    TODO(rqi): do matrix multiplication only once for each projection.
+
+    """
+
+    def __init__(self, calib_filepath, from_video=False):
+        if from_video:
+            calibs = self.read_calib_from_video(calib_filepath)
+        else:
+            calibs = self.read_calib_file(calib_filepath)
+        # Projection matrix from rect camera coord to image2 coord
+        self.P = calibs['P2']
+        self.P = np.reshape(self.P, [3, 4])
+        self.P_3 = calibs['P3']
+        self.P_3 = np.reshape(self.P_3, [3, 4])
+        # Rigid transform from Velodyne coord to reference camera coord
+        self.V2C = calibs['Tr_velo_to_cam']
+        self.V2C = np.reshape(self.V2C, [3, 4])
+        self.C2V = inverse_rigid_trans(self.V2C)
+        # Rotation from reference camera coord to rect camera coord
+        self.R0 = calibs['R0_rect']
+        self.R0 = np.reshape(self.R0, [3, 3])
+
+        # Camera intrinsics and extrinsics
+        self.c_u = self.P[0, 2]
+        self.c_v = self.P[1, 2]
+        self.f_u = self.P[0, 0]
+        self.f_v = self.P[1, 1]
+        self.b_x = self.P[0, 3] / (-self.f_u)  # relative
+        self.b_y = self.P[1, 3] / (-self.f_v)
+
+    def read_calib_file(self, filepath):
+        """
+        Read in a calibration file and parse into a dictionary.
+        Ref: https://github.com/utiasSTARS/pykitti/blob/master/pykitti/utils.py
+        :param filepath:
+        :return:
+        """
+        data = {}
+        with open(filepath, 'r') as f:
+            for line in f.readlines():
+                line = line.rstrip()
+                if len(line) == 0: continue
+                key, value = line.split(':', 1)
+                # The only non-float values in these files are dates, which
+                # we don't care about anyway
+                try:
+                    data[key] = np.array([float(x) for x in value.split()])
+                except ValueError:
+                    pass
+        return data
+
+    def read_calib_from_video(self, calib_root_dir):
+        ''' Read calibration for camera 2 from video calib files.
+            there are calib_cam_to_cam and calib_velo_to_cam under the calib_root_dir
+        '''
+        data = {}
+        cam2cam = self.read_calib_file(os.path.join(calib_root_dir, 'calib_cam_to_cam.txt'))
+        velo2cam = self.read_calib_file(os.path.join(calib_root_dir, 'calib_velo_to_cam.txt'))
+        Tr_velo_to_cam = np.zeros((3, 4))
+        Tr_velo_to_cam[0:3, 0:3] = np.reshape(velo2cam['R'], [3, 3])
+        Tr_velo_to_cam[:, 3] = velo2cam['T']
+        data['Tr_velo_to_cam'] = np.reshape(Tr_velo_to_cam, [12])
+        data['R0_rect'] = cam2cam['R_rect_00']
+        data['P2'] = cam2cam['P_rect_02']
+        return data
+
+    def cart2hom(self, pts_3d):
+        """
+        X (n,3) :--> (X|1) (n, 4)
+        :param pts_3d: nx3 points in Cartesian
+        :return: nx4 points in Homogeneous by pending 1
+        """
+        n = pts_3d.shape[0]
+        pts_3d_hom = np.hstack((pts_3d, np.ones((n, 1))))
+        return pts_3d_hom
+
+    # ===========================
+    # ------- 3d to 3d ----------
+    # ===========================
+    def project_velo_to_ref(self, pts_3d_velo):
+        """
+        project a 3D point from velo coord into ref cam coord
+        :param pts_3d_velo: 3d point in velo coord (n, 3)
+        :return: (n, 3) points into ref cam coord
+        V_ref = (V_velo) * (V2C).T
+        V2C == Tr_velo_to_cam (3x4)
+        """
+        pts_3d_velo = self.cart2hom(pts_3d_velo)  # nx4
+        return np.dot(pts_3d_velo, np.transpose(self.V2C))
+
+    def project_ref_to_velo(self, pts_3d_ref):
+        """
+        project a 3D point from ref cam coord into velo coord
+        :param pts_3d_ref:
+        :return:
+        V_rect = V_ref * (C2V).T
+        C2V == Tr_cam_to_velo (3x4)
+        """
+        pts_3d_ref = self.cart2hom(pts_3d_ref)  # nx4
+        return np.dot(pts_3d_ref, np.transpose(self.C2V))
+
+    def project_rect_to_ref(self, pts_3d_rect):
+        """
+        Project a point 3D in rectified cam coord into reference cam coord (left gray cam)
+        Input and Output are nx3 points
+        :param pts_3d_rect:
+        :return:
+        V_ref = ((R0)^-1 * (V_rect).T).T
+        R0 (3x3)
+        """
+        return np.transpose(np.dot(np.linalg.inv(self.R0), np.transpose(pts_3d_rect)))
+
+    def project_ref_to_rect(self, pts_3d_ref):
+        """
+        project a 3D point from ref cam coord into rect cam coord
+        :param pts_3d_velo: 3d point in ref cam coord (n, 3)
+        :return: (n, 3) points into rect cam coord
+        V_rect = (R0) * (V_ref).T
+        R0 (3x3)
+        """
+        ''' Input and Output are nx3 points '''
+        return np.transpose(np.dot(self.R0, np.transpose(pts_3d_ref)))
+
+    def project_rect_to_velo(self, pts_3d_rect):
+        """
+        project a 3D point from rect cam coord into velo coord
+        :param pts_3d_velo: 3d point in rect cam coord (n, 3)
+        :return: (n, 3) points into velo coord
+        """
+        pts_3d_ref = self.project_rect_to_ref(pts_3d_rect)
+        return self.project_ref_to_velo(pts_3d_ref)
+
+    def project_velo_to_rect(self, pts_3d_velo):
+        """
+        project a 3D point from velo coord into rect cam coord
+        :param pts_3d_velo: 3d point in velo coord (n, 3)
+        :return: (n, 3) points into rect cam coord
+        velo --> ref --> rect
+        """
+        pts_3d_ref = self.project_velo_to_ref(pts_3d_velo)
+        return self.project_ref_to_rect(pts_3d_ref)
+
+    # ===========================
+    # ------- 3d to 2d ----------
+    # ===========================
+    def project_rect_to_image(self, pts_3d_rect, depth=False, side='left'):
+        """
+        project a 3D point from rect cam coord into image (left color cam)
+        :param pts_3d_rect: 3d point in rect cam coord (n, 3)
+        :return:
+        V_img = ((V_rect.T)|1) * P2.T
+        """
+        pts_3d_rect = self.cart2hom(pts_3d_rect)
+        if side=='left':
+            pts_2d = np.dot(pts_3d_rect, np.transpose(self.P))  # nx3
+        else:
+            pts_2d = np.dot(pts_3d_rect, np.transpose(self.P_3))  # nx3
+        # scale projected point center in image plane to top left corner in image
+        pts_2d[:, 0] /= pts_2d[:, 2]
+        pts_2d[:, 1] /= pts_2d[:, 2]
+        if depth==True:
+            return pts_2d
+        return pts_2d[:, 0:2]
+
+    def project_velo_to_image(self, pts_3d_velo, depth=False, side='left'):
+        """
+        project a 3D point from velo coord into image coord
+        :param pts_3d_velo: 3d point in velo coord (n, 3)
+        :return: (n, 2) points in image2 coord.
+        """
+        pts_3d_rect = self.project_velo_to_rect(pts_3d_velo)
+        return self.project_rect_to_image(pts_3d_rect, depth=depth) if side=='left' else self.project_rect_to_image(pts_3d_rect, depth=depth, side=side)
+
+    # ===========================
+    # ------- 2d to 3d ----------
+    # ===========================
+    def project_image_to_rect(self, uv_depth):
+        ''' Input: nx3 first two channels are uv, 3rd channel
+                   is depth in rect camera coord.
+            Output: nx3 points in rect camera coord.
+        '''
+        n = uv_depth.shape[0]
+        x = ((uv_depth[:, 0] - self.c_u) * uv_depth[:, 2]) / self.f_u + self.b_x
+        y = ((uv_depth[:, 1] - self.c_v) * uv_depth[:, 2]) / self.f_v + self.b_y
+        pts_3d_rect = np.zeros((n, 3))
+        pts_3d_rect[:, 0] = x
+        pts_3d_rect[:, 1] = y
+        pts_3d_rect[:, 2] = uv_depth[:, 2]
+        return pts_3d_rect
+
+    def project_image_to_velo(self, uv_depth):
+        pts_3d_rect = self.project_image_to_rect(uv_depth)
+        return self.project_rect_to_velo(pts_3d_rect)
