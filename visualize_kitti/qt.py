4ac730df2b13e057d7af754d9aed1df392e10644
Minh <mainguyenanhminh1996@gmail.com>
2021-01-14 17:07:20 +0100

    1st commit

---
+++ visualize_kitti/qt.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 file dialogs - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        # self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        file = self.openFileNameDialog()
+        # self.openFileNamesDialog()
+        # self.saveFileDialog()
+        
+        # self.show()
+        return file# sys.exit()
+    
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+        return fileName
